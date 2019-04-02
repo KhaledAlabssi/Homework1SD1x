bc75008ff6f5a9f62fdbae953c008a298c5945db
KhaledAlabssi <khaled.absi5@gmail.com>
2019-04-02 17:44:36 +0200

    Add boolean place

--- src/whackamole/WhackAMole.java
+++ src/whackamole/WhackAMole.java
@@ -18,4 +18,23 @@ public class WhackAMole {
         this.moleGrid = new char[gridDimension][gridDimension];
 
     }
+    // Create the following methods.
+    // boolean place(int x, int y) Given a location, place a mole at that location, return true if you can.
+    // void whack(int x, int y) given a location, take a whack at that location, if that location contain a mole,
+    // void printGridToUser() print the grid without showing where moles are,
+    // void printGrid() print the grid completely.
+    public boolean place(int x, int y) {
+        // check if the slot is empty?
+        if (this.moleGrid[x][y] != 'M') {
+            this.moleGrid[x][y] = 'M';
+            this.molesLeft ++;
+            System.out.println("Still there " + this.molesLeft + " mole to go.");
+            return true;
+        }
+        else {
+            System.out.println("Here is a mole!.");
+            return false;
+        }
+    }
+
 }
