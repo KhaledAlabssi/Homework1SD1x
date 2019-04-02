9b6fd36bd0c08146ebf7bb2f3519d606629a183c
KhaledAlabssi <khaled.absi5@gmail.com>
2019-04-02 16:54:25 +0200

    Add the file and get ready for the homework.

---
+++ src/whackamole/WhackAMole.java
@@ -0,0 +1,12 @@
+package whackamole;
+
+
+// Create a class called WhackAMole.
+public class WhackAMole {
+    // Create instance variables called: score, molesLeft and attemptsLeft.
+    int score;
+    int molesLeft;
+    int attemptsLeft;
+    // and one called: moleGrid whic is a 2-dimensional array of chars.
+    char [][] moleGrid;
+}
