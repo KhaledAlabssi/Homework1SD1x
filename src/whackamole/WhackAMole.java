46177dfdd2f5aaa471aeba91bb10c4f916b24af6
KhaledAlabssi <khaled.absi5@gmail.com>
2019-04-02 17:08:52 +0200

    Add the constructor

--- src/whackamole/WhackAMole.java
+++ src/whackamole/WhackAMole.java
@@ -1,12 +1,21 @@
 package whackamole;
 
 
-// Create a class called WhackAMole.
+    // Create a class called WhackAMole.
 public class WhackAMole {
     // Create instance variables called: score, molesLeft and attemptsLeft.
     int score;
     int molesLeft;
     int attemptsLeft;
-    // and one called: moleGrid whic is a 2-dimensional array of chars.
+    // and one called: moleGrid which is a 2-dimensional array of chars.
     char [][] moleGrid;
+
+    // Constructor for the game, make sure to initialize the moleGrid with the appropriate character.
+    WhackAMole(int numAttempts, int gridDimension) {
+        this.score = 0;
+        this.molesLeft = 0;
+        this.attemptsLeft = 0;
+        this.moleGrid = new char[gridDimension][gridDimension];
+
+    }
 }
